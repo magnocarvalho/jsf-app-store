6d2e8a93f378864d209c099939e5adb6a766a1e4
Magno Carvalho <magnocarv@hotmail.com>
2018-11-21 03:16:20 -0200

    livro dao admin

---
+++ src/main/java/br/com/xstore/loja/beans/AdminLivrosBean.java
@@ -0,0 +1,61 @@
+package br.com.xstore.loja.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
+
+import br.com.xstore.loja.daos.AutorDao;
+import br.com.xstore.loja.daos.LivroDao;
+import br.com.xstore.loja.models.Autor;
+import br.com.xstore.loja.models.Livro;
+
+@Named
+@RequestScoped
+public class AdminLivrosBean {
+	
+	private Livro livro = new Livro();
+	
+	@Inject
+	private LivroDao dao;
+	@Inject
+	private AutorDao autorDao;
+	
+	private List<Integer> autoresId = new ArrayList<>();
+
+
+	@Transactional
+	public String salvar() {
+		for (Integer autorId : autoresId) {
+			livro.getAutores().add(new Autor(autorId));
+		}
+		dao.salvar(livro);
+		System.out.println("Livro Cadastrado: " + livro);
+		
+		return "/livros/lista?faces-redirect=true";
+	}
+
+	public List<Autor> getAutores() {
+		return autorDao.listar();
+	}
+	
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+
+	public List<Integer> getAutoresId() {
+		return autoresId;
+	}
+
+	public void setAutoresId(List<Integer> autoresId) {
+		this.autoresId = autoresId;
+	}
+	
+}
